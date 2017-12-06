33cce12e8ee3a5373c7264c3178be9c42a3174a3
feifeiyuan <33796779+feifeiyuan@users.noreply.github.com>
2017-12-06 22:46:10 +0800

    Update DeviceCommon.mk

--- boot_loading/DeviceCommon.mk
+++ boot_loading/DeviceCommon.mk
@@ -1,3 +1,6 @@
 # 编译时拷贝至目标目录
 PRODUCT_COPY_FILES += \
 		       device/sprd/xxx/common/myshell.sh:system/bin/myshell.sh
+原始文件位置：device/sprd/xxx/common/myshell.sh
+目标文件位置：system/bin/myshell.sh
+备注：拷贝文件一般放在device下，且需要编译systemimage
