41176c528ec1eb53d7a064e2b93c0b98ab2118f0
feifeiyuan <33796779+feifeiyuan@users.noreply.github.com>
2017-12-06 21:27:09 +0800

    Update create_sys_node.c

--- create_sys_node.c
+++ create_sys_node.c
@@ -13,3 +13,46 @@ static ssize_t sysfs_demo_store_cpu_total(struct kobject *kobj, struct kobj_attr
 	sprintf(cpu_total_buf, "%s", buf);
 	return n;
 }
+static struct kobj_attribute cpu_total_attribute =
+    __ATTR(cpu_total, S_IWUSR|S_IRUGO, sysfs_demo_show_cpu_total, sysfs_demo_store_cpu_total);
+
+
+static struct attribute *sysfs_demo_attributes[] = {
+    &cpu_total_attribute.attr,
+    NULL
+};
+
+static const struct attribute_group sysfs_demo_attr_group = {
+    .attrs = sysfs_demo_attributes,
+};   
+
+static int __init sysfs_demo_init(void){
+    if ((k_obj = kobject_create_and_add("cpu_loading", NULL)) == NULL ) {
+        printk("sysfs_demo sys node create error \n");
+        goto out;
+    }
+
+    if(sysfs_create_group(k_obj, &sysfs_demo_attr_group) ) {
+        printk("sysfs_create_group failed\n");
+        goto out2;
+    }
+
+    return 0;
+out2:
+    kobject_put(k_obj);
+out:
+    return -1;
+}
+
+static void __exit sysfs_demo_exit(void){
+    if (k_obj) {
+        sysfs_remove_group(k_obj, &sysfs_demo_attr_group);
+        kobject_put(k_obj);
+    }
+    return;
+}
+
+module_init(sysfs_demo_init);
+module_exit(sysfs_demo_exit);
+
+MODULE_LICENSE("GPL");
